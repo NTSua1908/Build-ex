0222b96370f525f8f417d1c49704e989872e6719
nguyenthiensua <nguyenthiensua@teams.airlab.dev>
2024-06-06 10:11:57 +0700

    Update path

--- helper/google.helper.js
+++ helper/google.helper.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.drive = void 0;
 const path_1 = __importDefault(require("path"));
 const googleapis_1 = require("googleapis");
-const KEYFILEPATH = path_1.default.join("/etc/secrets/", "../experience-424602-df66ebfd0638.json");
+const KEYFILEPATH = path_1.default.join("/etc/secrets/", "experience-424602-df66ebfd0638.json");
 const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
 const auth = new googleapis_1.google.auth.GoogleAuth({
     keyFile: KEYFILEPATH,
